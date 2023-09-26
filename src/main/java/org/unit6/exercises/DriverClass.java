d4f05e86feaf100454826953aa83ffd88c65eb34
N E I L  O H E N E <54632119+dvc-77@users.noreply.github.com>
2023-09-26 16:24:27 +0000

    update craps game

--- src/main/java/org/unit6/exercises/DriverClass.java
+++ src/main/java/org/unit6/exercises/DriverClass.java
@@ -6,7 +6,22 @@ import static java.lang.System.*;
 public class DriverClass {
     public static void main(String[] args) {
         CrapsDiceGame game = new CrapsDiceGame();
-        game.rollDie();
+        String status = game.gameStatus();
+
+        if(status != "CONTINUE") {
+            out.printf("Player has %s%n", status);
+        }else {
+            while (status == "CONTINUE") {
+                status = game.gameStatus();
+                if (status != "CONTINUE") {
+                    out.printf("Player has %s%n", status);
+                    break;
+                }else {
+                    out.printf("Player will %s%n", status);
+                }
+            }
+        }
+
 
 
     }
