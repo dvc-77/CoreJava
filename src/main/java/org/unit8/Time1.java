8d2ec951ddb7b0120cdf12ca332150e048966822
N E I L  O H E N E <54632119+dvc-77@users.noreply.github.com>
2023-09-29 07:57:29 +0000

    unit8: new method(displayTime)

---
+++ src/main/java/org/unit8/Time1.java
@@ -0,0 +1,37 @@
+package org.unit8;
+
+public class Time1 {
+    private int hour;   // 0 - 23
+    private int minute; // 0 - 59
+    private int second; // 0 - 59
+
+    // set a new time value using universal time
+    // exception is thrown if the hour, minute or second is invalid
+
+    public void setTime(int hour, int minute, int second) {
+        if (hour < 0 || hour >= 24 || minute < 0 || second < 0 || second >= 60) {
+            throw new IllegalArgumentException("hour, minute and/or second was out");
+        }
+
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+
+    }
+
+    public String toUniversalString() {
+        return String.format("%02d:%02d:%02d", hour, minute, second);
+    }
+
+    public String toString() {
+        return String.format("%02d:%02d:%02d %s",
+                ((hour == 0 || hour == 12) ? 12 : (hour > 12 ? (hour-12): hour)), minute, second,
+                (hour < 12 ? "AM":"PM")
+        );
+    }
+
+    public String displayTime(){
+        return String.format("Standard Time: %s%nUniversal Time: %s", toString(), toUniversalString());
+    }
+
+}
---
+++ src/main/java/org/unit8/Time1Test.java
@@ -0,0 +1,18 @@
+package org.unit8;
+
+public class Time1Test {
+    public static void main(String[] args) {
+        Time1 time = new Time1();
+
+        try {
+            time.setTime(23, 56, 23);
+        } catch (Exception e) {
+            System.out.println(String.format("Exception: %s", e));
+            System.exit(0);
+        }
+//        System.out.println(time.toUniversalString());
+//
+//        System.out.println(time);
+        System.out.println(time.displayTime());
+    }
+}
