f1e6bde7dce853c5e186f056e3d2d1f570cc8928
N E I L  O H E N E <54632119+dvc-77@users.noreply.github.com>
2023-10-08 23:41:23 +0000

    inheritance

--- src/main/java/org/unit8/Interest.java
+++ src/main/java/org/unit8/Interest.java
@@ -7,17 +7,22 @@ import java.text.NumberFormat;
 public class Interest {
 
     public static void main(String[] args) {
-        BigDecimal principal = BigDecimal.valueOf(1250);
-        BigDecimal rate = BigDecimal.valueOf(0.05);
 
-        out.printf("%s%20s%n", "Year", "Amount");
+//        BigDecimal principal = BigDecimal.valueOf(1250);
+//        BigDecimal rate = BigDecimal.valueOf(0.05);
+//
+//        out.printf("%s%20s%n", "Year", "Amount");
+//
+//        for(int year = 1; year <= 10; year++)  {
+//            BigDecimal amount = principal.multiply(rate.add(BigDecimal.valueOf(year)));
+//            out.printf("%4d%20s%n", year,
+//                    NumberFormat.getCurrencyInstance().format(amount));
+//        }
 
-        for(int year = 1; year <= 10; year++) {
-            BigDecimal amount = principal.multiply(rate.add(BigDecimal.valueOf(year)));
-            out.printf("%4d%20s%n", year,
-                    NumberFormat.getCurrencyInstance().format(amount));
+        String phrase = "This-is-a-Test";
+        String fmtString = phrase.replace("-", "").toLowerCase();
 
-        }
+        out.println(fmtString);
 
-    }
+   }
 }
